758773e04e345479053a17465dc9c3cff91a9456
heycristhian <heycristhian@gmail.com>
2020-02-17 20:36:26 -0300

    Fazendo relatório de atividade

--- SGTT/Forms/frmRelatorioMensalAtividades.Designer.cs
+++ SGTT/Forms/frmRelatorioMensalAtividades.Designer.cs
@@ -29,67 +29,92 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.mtbDataFinal = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mtbDataInicial = new System.Windows.Forms.MaskedTextBox();
+            this.btnGerar = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.maskedTextBox2);
+            this.groupBox1.Controls.Add(this.mtbDataFinal);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.mtbDataInicial);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(323, 133);
+            this.groupBox1.Size = new System.Drawing.Size(323, 104);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Período:";
             // 
-            // maskedTextBox1
+            // label2
             // 
-            this.maskedTextBox1.Location = new System.Drawing.Point(16, 58);
-            this.maskedTextBox1.Mask = "00/00/0000";
-            this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox1.TabIndex = 0;
-            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(188, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data Inicial:";
+            // 
+            // mtbDataFinal
+            // 
+            this.mtbDataFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbDataFinal.Location = new System.Drawing.Point(191, 50);
+            this.mtbDataFinal.Mask = "00/00/0000";
+            this.mtbDataFinal.Name = "mtbDataFinal";
+            this.mtbDataFinal.Size = new System.Drawing.Size(100, 24);
+            this.mtbDataFinal.TabIndex = 2;
+            this.mtbDataFinal.ValidatingType = typeof(System.DateTime);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 39);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 31);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.Size = new System.Drawing.Size(71, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Data Inicial:";
             // 
-            // label2
+            // mtbDataInicial
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(172, 39);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(63, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Data Inicial:";
+            this.mtbDataInicial.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.mtbDataInicial.Location = new System.Drawing.Point(35, 50);
+            this.mtbDataInicial.Mask = "00/00/0000";
+            this.mtbDataInicial.Name = "mtbDataInicial";
+            this.mtbDataInicial.Size = new System.Drawing.Size(100, 24);
+            this.mtbDataInicial.TabIndex = 0;
+            this.mtbDataInicial.ValidatingType = typeof(System.DateTime);
             // 
-            // maskedTextBox2
+            // btnGerar
             // 
-            this.maskedTextBox2.Location = new System.Drawing.Point(172, 58);
-            this.maskedTextBox2.Mask = "00/00/0000";
-            this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
-            this.maskedTextBox2.TabIndex = 2;
-            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            this.btnGerar.BackColor = System.Drawing.Color.Lime;
+            this.btnGerar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGerar.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.btnGerar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnGerar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnGerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerar.ForeColor = System.Drawing.Color.White;
+            this.btnGerar.Location = new System.Drawing.Point(199, 122);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(136, 29);
+            this.btnGerar.TabIndex = 1;
+            this.btnGerar.Text = "GERAR RELATÓRIO";
+            this.btnGerar.UseVisualStyleBackColor = false;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
             // 
             // frmRelatorioMensalAtividades
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(934, 489);
+            this.Controls.Add(this.btnGerar);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmRelatorioMensalAtividades";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -106,8 +131,9 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.MaskedTextBox mtbDataFinal;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox mtbDataInicial;
+        private System.Windows.Forms.Button btnGerar;
     }
 }
--- SGTT/Forms/frmRelatorioMensalAtividades.cs
+++ SGTT/Forms/frmRelatorioMensalAtividades.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,9 @@ namespace SGAP.Forms
 {
     public partial class frmRelatorioMensalAtividades : Form
     {
+        public string inicio { get; set; }
+        public string final { get; set; }
+
         public frmRelatorioMensalAtividades()
         {
             InitializeComponent();
@@ -19,7 +24,198 @@ namespace SGAP.Forms
 
         private void frmRelatorioMensalAtividades_Load(object sender, EventArgs e)
         {
-            label1.Size = new System.Drawing.Size(8, 8);
+            inicio = DateTime.Now.AddDays(-(DateTime.Now.Day - 1)).ToString("dd-MM-yyyy");
+            final = DateTime.Now.AddDays(-(DateTime.Now.Day - 1)).AddMonths(1).AddDays(-1).ToString("dd-MM-yyyy");
+            mtbDataInicial.Text = inicio;
+            mtbDataFinal.Text = final;
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            string folder = @"c:\Relatórios";
+            string arquivo = @"C:\Relatórios\Atividade Mensal (" + inicio + " - " + final + ").xlsx";
+
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+
+            if (File.Exists(arquivo))
+            {
+                //Fazer tratamento
+                File.Delete(arquivo);
+            }
+
+            FileInfo caminhoNomeArquivo = new FileInfo(arquivo);
+            ExcelPackage arquivoExcel = new ExcelPackage(caminhoNomeArquivo);
+
+            int linha = 0;
+            int coluna = 1;
+            int contador = 0;
+
+            // CRIANDO (ADD) uma planilha neste arquivo e obtendo a referência para meu código operá-la.
+            ExcelWorksheet planilha = arquivoExcel.Workbook.Worksheets.Add("Plan1");
+
+            planilha.Cells[++linha, coluna].Value = "ITEM";
+            planilha.Cells[linha, ++coluna].Value = "DESCRIÇÃO";
+            planilha.Cells[linha, ++coluna].Value = "ALIMENTOS";
+            planilha.Cells[linha, ++coluna].Value = "SAÚDE";
+            planilha.Cells[linha, ++coluna].Value = "HABITAÇÃO";
+            planilha.Cells[linha, ++coluna].Value = "PRODUTOS";
+            planilha.Cells[linha, ++coluna].Value = "SERVIÇOS PRIVADOS";
+            planilha.Cells[linha, ++coluna].Value = "SERVIÇOS ESSENCIAIS";
+            planilha.Cells[linha, ++coluna].Value = "ASSUNTOS FINANCEIROS";
+            planilha.Cells[linha, ++coluna].Value = "TOTAL";
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Simples Consulta";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Atendimento Preliminar";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "CIPs emitidas";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "CIPs finalizadas com acordo";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "CIPs finalizadas com outras baixas (encerradas, canceladas, consulta concluída";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Reclamações abertas no retorno da CIP (CIPs finalizadas com \"Abertura de reclamação\")";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Reclamações abertas sem emissão de CIP";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Reclamações finalizadas como Atendidas";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Reclamações finalizadas como NÃO Atendidas";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Reclamações finalizadas com outras baixas (encerradas, consulta fornecida)";
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Extra Procon";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = (++contador).ToString("D2");
+            planilha.Cells[linha, ++coluna].Value = "Nota Fiscal Paulista";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            planilha.Cells[++linha, coluna = 1].Value = "Total Geral";
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = null;
+            planilha.Cells[linha, ++coluna].Value = 0;
+
+            arquivoExcel.Save();
+            arquivoExcel.Dispose();
+
+            System.Diagnostics.Process.Start(arquivo);
         }
     }
 }
